f61241b4c10369e63be29ec10872b7a25e399536
samuelngjy <165895851+samuelngjy@users.noreply.github.com>
2024-06-06 22:22:40 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+﻿$(document).on("click", "a", function (e) {
+    e.preventDefault();
+    var id = $(this).attr("href"),
+        topSpace = 30;
+    //alert(id);
+    $('html, body').animate({
+        scrollTop: $(id).offset().top - topSpace
+    }, 800);
+});
